7fc15cb0d0638131ff964a88daab074f628eca07
sucream <ghdskatnghds@gmail.com>
2019-01-13 19:46:46 +0900

    pos_tag function upgrade and add TF

--- nlp_study_with_jupyter/chap3/pos_tag.py
+++ nlp_study_with_jupyter/chap3/pos_tag.py
@@ -6,7 +6,8 @@ from urllib import parse  # 한글을 올바르게 전달하기 위해 인코딩
 pt = re.compile(r'(?P<text>.+?)\((?P<pos>.+):.+\)')
 # 주로(Noun: 4, 2) 같은 패턴을 text와 pos로 분리해서 사용하기 위한 정규식
 
-def twitter_pos(sentence, concat=False):
+
+def twitter_pos(sentence, concat=False, discard_stopwords=False, discard_verb=False):
     print('[', sentence, '] \n형태소 분석중...', end=' : ')
     sentence = parse.quote(sentence)  # 한글 인코딩
     # print(sentence) 어떻게 변경되는지 확인해보세요
@@ -31,6 +32,12 @@ def twitter_pos(sentence, concat=False):
     for i in response['tokens']:
         m = pt.match(i)
         if m:
+            if discard_verb:
+                if 'Verb' in m.group('pos'):
+                    continue
+            if discard_stopwords:
+                if m.group('pos') in ('Punctuation', 'Josa'):
+                    continue
             if concat:
                 tokens.append(m.group('text') + '/' + m.group('pos'))
             else:
